8733f76a3ac497ae71ad6c2f6ad254029ff2faf5
uGuardian <Sean.Heavysege@gmail.com>
2022-05-19 15:30:27 -0600

    Added new scripts

---
+++ DiceCardAbility_TABinBin_PowerNullArea.cs
@@ -0,0 +1,15 @@
+﻿namespace Classic_Teal
+{
+	public class DiceCardAbility_CTABinBin_PowerNullArea : DiceCardAbilityBase
+	{
+		public static string Desc = "[On Hit] Target's dice Power cannot be influenced by any effects next Scene";
+		public override void OnSucceedAreaAttack(BattleUnitModel target)
+		{
+			if (target == null)
+			{
+				return;
+			}
+			target.bufListDetail.AddKeywordBufByCard(KeywordBuf.NullifyPower, 1, base.owner);
+		}
+	}
+}
